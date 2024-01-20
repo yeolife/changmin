a919535814c40a5db122ce574b873909b5f94c0d
여창민 <82012857+yeolife@users.noreply.github.com>
2024-01-20 16:44:38 +0900

    Update 1238.java
    [BOJ] 여행 1238 여창민

--- changmin/1238.java
+++ changmin/1238.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.OutputStreamWriter; 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
