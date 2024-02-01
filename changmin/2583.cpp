161d7248294b687bc4b01327b78dd92914f918fd
여창민 <82012857+yeolife@users.noreply.github.com>
2024-02-01 10:54:10 +0900

    [BOJ] 영역 구하기 2583 여창민

--- changmin/2583.cpp
+++ changmin/2583.cpp
@@ -46,14 +46,14 @@ int bfs(int x, int y) {
 int main() {
     ios::sync_with_stdio(0); cin.tie(NULL);
 
-    cin >> n >> m >> k;
+    cin >> m >> n >> k;
 
     int x1, y1, x2, y2;
     for(int i = 0; i < k; i++) {
         cin >> x1 >> y1 >> x2 >> y2;
 
-        for(int x = y1; x < y2; x++) {
-            for(int y = x1; y < x2; y++)
+        for(int x = x1; x < x2; x++) {
+            for(int y = y1; y < y2; y++)
                 board[x][y] = true;
         }
     }
